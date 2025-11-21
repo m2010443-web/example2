087df8738955123fe61ca1e68e3b3cd27cc764c0
m2010443-web <m2010443@edu.misis.ru>
2025-11-21 15:24:01 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+Sales Analytics Platform - Source Package
+"""
+
+from . import data_loader
+from . import analysis
+from . import plotting
+
+__version__ = "3.0.0"
+__all__ = ['data_loader', 'analysis', 'plotting']
